c7596ec8876444b6236c77b0d9602ab1b98eb8c0
oliviaeugenio <oli@eugenio.software>
2024-12-29 19:03:33 -0500

    Removed an unneeded js file

--- search.js
+++
@@ -1,6 +0,0 @@
-
-
-// Now we must make a listener and override default behavior for the search button.
-// first we will find the search button.
-const searchContainer = document.getElementById('search-container');
-console.log(searchContainer); // Logs the <div> element

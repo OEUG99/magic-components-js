8c27aa8d8893cb41efa857a16fa31c96abc1757b
oliviaeugenio <oli@eugenio.software>
2025-01-04 23:10:04 -0500

    refactoring the vdom to make it more clear

---
+++ src/lib/MagicDOM.js
@@ -0,0 +1,17 @@
+import AbstractComponent from "./AbstractComponent";
+
+export default class ComponentHandler {
+    constructor() {
+        // We'll load in our dynamic content on load.
+        document.addEventListener("DOMContentLoaded", async () => {
+            console.log("Loading Components...");
+
+
+        })
+
+    }
+
+    
+
+
+}
